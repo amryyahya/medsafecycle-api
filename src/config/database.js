e500e82c8e6d1c54e698fe9d290974dec290fee2
amryyahya <amryyahya3701@gmail.com>
2023-06-15 18:53:38 +0700

    update database

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ const medsafe_db = new Sequelize(
  process.env.DATABASE_USER,
  process.env.DATABASE_PASSWORD,
   {
-    host: '34.101.67.21',
+    host: process.env.DATABASE_HOST,
     dialect: 'mysql'
   }
 );
